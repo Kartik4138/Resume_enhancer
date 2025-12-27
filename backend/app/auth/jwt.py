6fe0f0b88937ccaab5e4aab7ede74894ecfe52ce
Kartik Singh <kartiksingh.7.rs@gmail.com>
2025-12-27 21:11:41 +0530

    Fixed jwt logic

--- backend/app/auth/jwt.py
+++ backend/app/auth/jwt.py
@@ -1,10 +1,13 @@
 from datetime import datetime, timedelta
 from jose import jwt
 import os
+from dotenv import load_dotenv
 
-SECRET_KEY = os.getenv("JWT_SECRET", "dev-secret-change-me")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 150
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = 15
 REFRESH_TOKEN_EXPIRE_DAYS = 7
 
 
--- backend/app/llm/gemini_client.py
+++ backend/app/llm/gemini_client.py
@@ -1,6 +1,9 @@
 import os
 from google import genai
 from google.genai import types
+from dotenv import load_dotenv 
+
+load_dotenv()
 
 API_KEY = os.getenv("GEMINI_API_KEY")
 if not API_KEY:
