856fa67acde26dd6948ea5992c29b06df5f24da1
Kartik Singh <kartiksingh.7.rs@gmail.com>
2025-12-27 21:44:07 +0530

    Fixed variables

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,10 +1,14 @@
+import os
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
 from app.db.base import Base
+from dotenv import load_dotenv 
 
-DATABASE_URL = "postgresql+asyncpg://postgres:root@localhost:5432/ats_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(
     DATABASE_URL,
